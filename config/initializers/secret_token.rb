5c6dc59658c982544562b9c66bd0f2a144af4468
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2012-09-14 22:48:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musicgrader::Application.config.secret_token = '7dbb30039b2a79bb348712df52ee073e16ccbde221ab60f635d9144cdc2dcdc25291cf54aa8fcb749caaf64b3a4ba513185f6c9ae0516471c7f1f9679e6e76c9'
