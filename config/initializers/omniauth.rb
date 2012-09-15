e2da360b3d3926ad4b549ce7d318fba031a1f598
Xinyi Chen <xinyic2015@gmail.com>
2012-09-15 10:20:33 -0400

    authentication

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  # The following is for facebook
-  provider :facebook, '239666966156370', 'e5690f028390d7241c0206cc3382fedc'
- 
-  # If you want to also configure for additional login services, they would be configured here.
-end
