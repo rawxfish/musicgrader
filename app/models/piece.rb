00a20b56263cccfa91b44a26f6710e757f802727
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2012-09-15 12:05:25 -0400

    add t-test

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,25 +2,28 @@ class Piece
 	include Mongoid::Document
 	field :name, type: String
 	field :user_id, type: Integer
+	field :correct_tempo, type: Float
 	field :beat_data, type: String
 	belongs_to :user, class_name:'User', inverse_of: :pieces
 
+
+	def size
+		File.open(beat_data, "r").readlines.size - 1
+	end
+
 	def mean
 		m = 0
-		n = -1
 		prev_data = File.open(beat_data, "r").readline
 		File.open(beat_data, "r").each_line do |data|
 			m = m + (data.to_f - prev_data.to_f)
 			prev_data = data
-			n = n + 1
 		end
-		m = m / n
+		m = m / size
 	end
 
 	def std_dev
 		m = mean
 		s = 0
-		n = 0
 		prev_data = File.open(beat_data, "r").readline
 
 		File.open(beat_data, "r").each_line do |data|
@@ -28,15 +31,13 @@ class Piece
 				d = (data.to_f - prev_data.to_f) - m
 				prev_data = data
 				s = s + d * d
-				puts s
-				n = n + 1
 			end
 		end
-		m = Math.sqrt(s / n)
+		m = Math.sqrt(s / size)
 	end
 
 	def z_test
-
+		z_score = (mean - correct_tempo) / (std_dev / Math.sqrt(size))
 	end
 
 
