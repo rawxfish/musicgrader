c1a5442bb1af180ec7f2c89152c16a8e1e956c3a
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2012-09-15 11:34:03 -0400

    add mean and stddev

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,6 +2,43 @@ class Piece
 	include Mongoid::Document
 	field :name, type: String
 	field :user_id, type: Integer
+	field :beat_data, type: String
 	belongs_to :user, class_name:'User', inverse_of: :pieces
+
+	def mean
+		m = 0
+		n = -1
+		prev_data = File.open(beat_data, "r").readline
+		File.open(beat_data, "r").each_line do |data|
+			m = m + (data.to_f - prev_data.to_f)
+			prev_data = data
+			n = n + 1
+		end
+		m = m / n
+	end
+
+	def std_dev
+		m = mean
+		s = 0
+		n = 0
+		prev_data = File.open(beat_data, "r").readline
+
+		File.open(beat_data, "r").each_line do |data|
+			unless prev_data == data
+				d = (data.to_f - prev_data.to_f) - m
+				prev_data = data
+				s = s + d * d
+				puts s
+				n = n + 1
+			end
+		end
+		m = Math.sqrt(s / n)
+	end
+
+	def z_test
+
+	end
+
+
 
 end
